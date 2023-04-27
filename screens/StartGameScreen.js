2f1d07cbdc9ca1fe2a1ba5a78c79fe32e0940eb0
allam <allamedd37@gmail.com>
2023-04-27 18:00:10 +0100

    style input number

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -3,18 +3,35 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen(){
 return (
     <View style={styles.inputContainer}>
-        <TextInput/>
-    <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>-
-
+        <TextInput style={styles.numberInput} maxLength={2} keyboardType="number-pad"/>
+    <PrimaryButton>Reset </PrimaryButton>
+        <PrimaryButton>Confirm </PrimaryButton>
     </View>
 );
 }
 export default StartGameScreen;
 const styles=StyleSheet.create({
     inputContainer:{
-        flex: 1,
         marginTop: 100,
         padding: 16,
+        marginHorizontal: 24,
+        backgroundColor: "#6F1E51",
+        borderRadius:8,
+        elevation : 4,
+        shadowColor : 'black',
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 6,
+        shadowOpacity: 0.25
+    },
+    numberInput:{
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor: "#f6e58d",
+        borderBottomWidth: 2,
+        color: "#f6e58d",
+        marginVertical: 8,
+        fontWeight:"bold",
+        textAlign: 'center',
     }
 })
